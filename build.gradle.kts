bc3ac852897e807ce021a23a3209952ff8f9caee
Yahor Usoltsau <yahor.usoltsau@itechart-group.com>
2021-10-08 01:48:52 +0300

    Setup integration tests

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,10 +3,36 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     kotlin("jvm") version "1.5.31"
     application
+    idea
 }
 
 group = "com.company"
 version = "0.0.1-SNAPSHOT"
+
+sourceSets {
+    create("testIntegration") {
+        kotlin {
+            compileClasspath += main.get().output
+            runtimeClasspath += main.get().output
+        }
+    }
+}
+
+val testIntegrationApi: Configuration by configurations.getting {
+    extendsFrom(configurations.testApi.get())
+}
+
+val testIntegrationImplementation: Configuration by configurations.getting {
+    extendsFrom(configurations.testImplementation.get())
+}
+
+val testIntegrationCompileOnly: Configuration by configurations.getting {
+    extendsFrom(configurations.testCompileOnly.get())
+}
+
+val testIntegrationRuntimeOnly: Configuration by configurations.getting {
+    extendsFrom(configurations.testRuntimeOnly.get())
+}
 
 repositories {
     mavenCentral()
@@ -18,16 +44,34 @@ dependencies {
     testImplementation(kotlin("test"))
     testImplementation("org.assertj:assertj-core:3.21.0")
     testImplementation("org.mockito:mockito-core:3.12.4")
-}
-
-tasks.test {
-    useJUnitPlatform()
+    testIntegrationImplementation("org.testcontainers:postgresql:1.16.0")
 }
 
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "11"
 }
 
+val testIntegration by tasks.registering(Test::class) {
+    description = "Runs integration tests."
+    group = "verification"
+    shouldRunAfter(tasks.test)
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
+
+tasks.check {
+    dependsOn(testIntegration)
+}
+
 application {
     mainClass.set("com.company.anonymizer.ApplicationKt")
+}
+
+idea {
+    module {
+        sourceDirs = emptySet()
+        testSourceDirs = sourceSets["testIntegration"].allSource.srcDirs
+    }
 }
